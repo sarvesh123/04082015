f26e46d02ad911bb09807adf8c35a4e5a3c13e02
Sarvesh Dev <sarvesh@sjinnovation.com>
2015-09-30 14:18:37 +0530

    css changes preview page: 1. show header buttons 2. toggle sidebar

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -34,7 +34,7 @@ angular.module('core').controller('HeaderController', ['$scope', '$state', 'Auth
     };
 
     $scope.showSidebarDefault = function ($state) {
-      var showArr = ['insights.edit'];
+      var showArr = ['insights.edit', 'insights.preview'];
       var currentName = $state.current.name;
 
       if ( showArr.indexOf(currentName) < 0 ) {
